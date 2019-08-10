da6d58279a88d68a95b208a6889e1dad3aec17c0
Advay Mengle (at Holvonix LLC) <advay-github@holvonix.com>
2019-08-10 01:10:05 -0700

    fix travis setup script

--- setup-travis.sh
+++ setup-travis.sh
@@ -15,42 +15,42 @@ curl --silent --show-error --fail -X POST \
   -H "Content-Type: application/json" \
   -H "Travis-API-Version: 3" \
   -H "Authorization: token $TRAVIS_TOKEN" \
-  -d '{ "env_var.name": "GIT_AUTHOR_EMAIL", "env_var.value": "$CI_AUTHOR_EMAIL", "env_var.public": false, "env_var.branch": "master" }' \
+  -d "{ \"env_var.name\": \"GIT_AUTHOR_EMAIL\", \"env_var.value\": \"$CI_AUTHOR_EMAIL\", \"env_var.public\": false, \"env_var.branch\": \"master\" }" \
   https://api.travis-ci.com/repo/$CI_REPO/env_vars
 
 curl --silent --show-error --fail -X POST \
   -H "Content-Type: application/json" \
   -H "Travis-API-Version: 3" \
   -H "Authorization: token $TRAVIS_TOKEN" \
-  -d '{ "env_var.name": "GIT_COMMITTER_EMAIL", "env_var.value": "$CI_AUTHOR_EMAIL", "env_var.public": false, "env_var.branch": "master" }' \
+  -d "{ \"env_var.name\": \"GIT_COMMITTER_EMAIL\", \"env_var.value\": \"$CI_AUTHOR_EMAIL\", \"env_var.public\": false, \"env_var.branch\": \"master\" }" \
   https://api.travis-ci.com/repo/$CI_REPO/env_vars
 
 curl --silent --show-error --fail -X POST \
   -H "Content-Type: application/json" \
   -H "Travis-API-Version: 3" \
   -H "Authorization: token $TRAVIS_TOKEN" \
-  -d '{ "env_var.name": "GIT_AUTHOR_NAME", "env_var.value": "$CI_AUTHOR_NAME", "env_var.public": false, "env_var.branch": "master" }' \
+  -d "{ \"env_var.name\": \"GIT_AUTHOR_NAME\", \"env_var.value\": \"$CI_AUTHOR_NAME\", \"env_var.public\": false, \"env_var.branch\": \"master\" }" \
   https://api.travis-ci.com/repo/$CI_REPO/env_vars
 
 curl --silent --show-error --fail -X POST \
   -H "Content-Type: application/json" \
   -H "Travis-API-Version: 3" \
   -H "Authorization: token $TRAVIS_TOKEN" \
-  -d '{ "env_var.name": "GIT_COMMITTER_NAME", "env_var.value": "$CI_AUTHOR_NAME", "env_var.public": false, "env_var.branch": "master" }' \
+  -d "{ \"env_var.name\": \"GIT_COMMITTER_NAME\", \"env_var.value\": \"$CI_AUTHOR_NAME\", \"env_var.public\": false, \"env_var.branch\": \"master\" }" \
   https://api.travis-ci.com/repo/$CI_REPO/env_vars
 
 curl --silent --show-error --fail -X POST \
   -H "Content-Type: application/json" \
   -H "Travis-API-Version: 3" \
   -H "Authorization: token $TRAVIS_TOKEN" \
-  -d '{ "env_var.name": "GH_TOKEN", "env_var.value": "$CI_GH_TOKEN", "env_var.public": false, "env_var.branch": "master" }' \
+  -d "{ \"env_var.name\": \"GH_TOKEN\", \"env_var.value\": \"$CI_GH_TOKEN\", \"env_var.public\": false, \"env_var.branch\": \"master\" }" \
   https://api.travis-ci.com/repo/$CI_REPO/env_vars
 
 curl --silent --show-error --fail -X POST \
   -H "Content-Type: application/json" \
   -H "Travis-API-Version: 3" \
   -H "Authorization: token $TRAVIS_TOKEN" \
-  -d '{ "env_var.name": "NPM_TOKEN", "env_var.value": "$CI_NPM_TOKEN", "env_var.public": false, "env_var.branch": "master" }' \
+  -d "{ \"env_var.name\": \"NPM_TOKEN\", \"env_var.value\": \"$CI_NPM_TOKEN\", \"env_var.public\": false, \"env_var.branch\": \"master\" }" \
   https://api.travis-ci.com/repo/$CI_REPO/env_vars
 
 rm .travis.env
